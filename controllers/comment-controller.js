d0616889a0a06595f65a35ec27f65dbd11920516
Candice Hall <hall.candice@outlook.com>
2022-06-17 16:57:23 -0400

    added reply functionality and routes + connected to front end

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,7 +3,6 @@ const {Comment, Pizza } = require('../models');
 const commentController = {
     // add comment to pizza
     addComment({ params, body }, res) {
-        console.log(body);
         Comment.create(body)
           .then(({ _id }) => {
             return Pizza.findOneAndUpdate(
@@ -12,6 +11,22 @@ const commentController = {
               { new: true }
             );
           })
+          .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => res.json(err));
+      },
+
+      addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+          { _id: params.commentId },
+          { $push: { replies: body } },
+          { new: true }
+        )
           .then(dbPizzaData => {
             if (!dbPizzaData) {
               res.status(404).json({ message: 'No pizza found with this id!' });
@@ -43,6 +58,16 @@ const commentController = {
             res.json(dbPizzaData);
           })
           .catch(err => res.json(err));
+      },
+
+      removeReply({ params}, res) {
+        Comment.findByIdAndUpdate(
+          { _id: params.commentId},
+          {$pull: {replies: { replyId: params.replyId } } },
+          {new: true }
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err=> res.json(err));
       }
   };
   
